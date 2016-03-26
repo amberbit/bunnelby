e68beba9a38b2842cd6a214589229367146ac26c
Hubert Łępicki <hubert.lepicki@amberbit.com>
2016-03-26 16:23:32 +0100

    add some test skeleton

--- spec/bunnelby_spec.rb
+++ spec/bunnelby_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
+require_relative './test_server'
+require_relative './test_client'
 
 describe Bunnelby do
   it 'has a version number' do
     expect(Bunnelby::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
+
---
+++ spec/test_server.rb
@@ -0,0 +1,3 @@
+class TestServer < Bunnelby::Server
+
+end
